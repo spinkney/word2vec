e6ccd6787cdeac381214aeec0c56c4e14302896d
spinkney <sean.pinkney@gmail.com>
2021-10-21 11:58:03 -0400

    updated sentence length

--- non_negative_word2vec.c
+++ non_negative_word2vec.c
@@ -21,7 +21,7 @@
 #define MAX_STRING 100
 #define EXP_TABLE_SIZE 1000
 #define MAX_EXP 6
-#define MAX_SENTENCE_LENGTH 1000
+#define MAX_SENTENCE_LENGTH 5000
 #define MAX_CODE_LENGTH 40
 
 const int vocab_hash_size = 30000000;  // Maximum 30 * 0.7 = 21M words in the vocabulary
